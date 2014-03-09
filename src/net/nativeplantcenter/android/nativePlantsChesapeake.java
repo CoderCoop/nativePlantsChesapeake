86ce36621d0e7020ff981b385e8b16c1fb60e2a2
Mike Chelen <michael.chelen@gmail.com>
2014-03-09 14:16:19 -0400

    create android project

---
+++ src/net/nativeplantcenter/android/nativePlantsChesapeake.java
@@ -0,0 +1,15 @@
+package net.nativeplantcenter.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class nativePlantsChesapeake extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
