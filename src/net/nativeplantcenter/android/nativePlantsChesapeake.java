9f6168ff8677c89a4f161955e5b0898551be6e2c
Mike Chelen <michael.chelen@gmail.com>
2014-03-20 23:13:25 -0400

    rollback java file to e9fba0d0bc

--- src/net/nativeplantcenter/android/nativePlantsChesapeake.java
+++ src/net/nativeplantcenter/android/nativePlantsChesapeake.java
@@ -7,47 +7,15 @@ import android.webkit.WebSettings;
 
 public class nativePlantsChesapeake extends Activity
 {
-  // create the webview object
-  protected WebView myWebView;
-   
-
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        myWebView = (WebView) findViewById(R.id.webview);
-        
+        WebView myWebView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        
-        
-        if (savedInstanceState == null) {
-        
-          myWebView.loadUrl("file:///android_asset/index.html");
-          
-        }
-        
+        myWebView.loadUrl("file:///android_asset/index.html");
     }
-    
-    // save the state of the webview
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-      super.onSaveInstanceState(outState);
-      // Save the state of the WebView
-      myWebView.saveState(outState);
-    }
-    
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState)
-    {
-      super.onRestoreInstanceState(savedInstanceState);
-   
-      // Restore the state of the WebView
-      myWebView.restoreState(savedInstanceState);
-    }
-
-    
 }
