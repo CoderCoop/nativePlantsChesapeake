0c1e4f7b98eee6e60b0f2a3102c020917991343e
Mike Chelen <michael.chelen@gmail.com>
2014-04-08 02:41:21 -0400

    formatting and css

--- assets/script.js
+++ assets/script.js
@@ -1,116 +1,113 @@
 $(document).ready(function () {   
    
-        var mydata; // create array to hold result data
+  var mydata; // create array to hold result data
        
-// search form
+  // search form
   $("form").on("submit", function (e) {
-        e.preventDefault(); // suppress form submit
-        
-        mydata = new Array(); // initialize with new array
-       
-        $('#mylist').empty(); // clear old results
-        
-        $.mobile.loading( 'show', {
-          text: "Loading...",
-          textVisible: true,
-          theme: "b",
-        });
+    
+    e.preventDefault(); // suppress form submit
+    
+    mydata = new Array(); // initialize with new array
+   
+    $('#mylist').empty(); // clear old results
+    
+    $.mobile.loading( 'show', {
+      text: "Loading...",
+      textVisible: true,
+      theme: "b",
+    });
 
-        var input = $('#query').val();
-        var api = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.nativeplantcenter.net%2F%3Fq%3Ddatabase%26count%3D-1%26keyword%3D" + input + "%22%20and%20xpath%3D'%2F%2Fdiv%5Bcontains(%40class%2C%22database_entry%20matrix_entry%22)%5D'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?";
+    var input = $('#query').val();
+    var api = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.nativeplantcenter.net%2F%3Fq%3Ddatabase%26count%3D-1%26keyword%3D" + input + "%22%20and%20xpath%3D'%2F%2Fdiv%5Bcontains(%40class%2C%22database_entry%20matrix_entry%22)%5D'&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?";
 
-        $.getJSON(api, {
-            //    tags: "mount rainier",
-            //    tagmode: "any",
-            //    format: "json"
-        })
-        .done(function (data) {
+    $.getJSON(api, {
+            format: "json"
+    })
+    
+    .done(function (data) {
+  
+      // handle case of 0 results found
+      if (!data.query.results) {
+        $('#mylist').html("<h3>No results found.</h3>");
+        $.mobile.loading( 'hide');
+        return;
+      }   
+        
+      // handle case of only 1 result returned     
+      if (typeof data.query.results.div[0] === 'undefined') {  
+        data.query.results.div = new Array(data.query.results.div);
+      }
       
-          // handle case of 0 results found
-          if (!data.query.results) {
-            $('#mylist').html("<h3>No results found.</h3>");
-            $.mobile.loading( 'hide');
-            return;
-          }
+      //iterate through each result          
+      $.each(data.query.results.div, function (i, div) {
         
-            
-          // handle case of only 1 result returned     
-          if (typeof data.query.results.div[0] === 'undefined') {
-          
-            data.query.results.div = new Array(data.query.results.div);
-          
-          }
-          
-          //iterate through each result          
-          $.each(data.query.results.div, function (i, div) {
-         
-            var entry = new Array();
-          
-            entry["url"]=div.div[3].div.div[0].a.href; //plant url
-            entry["thumb"]=div.div[0].a.img.src; //thumbnail image
-            entry["img"]=entry["thumb"].replace("thumbs/",""); //full size image  
-            entry["species"]=div.div[1].a.content; //scientific name
-            entry["name"]=div.div[2].p; //common name  
-            entry["commonNames"]=div.div[3].div.div[1].p.content; // all common names
-            entry["plantTypes"]=div.div[3].div.div[2].p; // plant types
-            entry["sunExposure"]=div.div[3].div.div[3].p; // sun exposure
-            entry["soilTexture"]=div.div[3].div.div[4].p; // soil texture
-            entry["soilMoisture"]=div.div[3].div.div[5].p; // soil moisture
-            entry["region"]=div.div[3].div.div[6].p; // region
-            
-            
-            // extract numeric plant id from url
-            plantid = entry["url"].split("/").pop(); 
-            
-            //remove leading punctuation
-            for (var x in entry) {
-              entry[x] = entry[x].replace(":  ","");
-              entry[x] = entry[x].replace(": ","");
-            }
-           
+        // create array to hold result data
+        var entry = new Array();
+        
+        // assign values from json
+        entry["url"]=div.div[3].div.div[0].a.href; //plant url
+        entry["thumb"]=div.div[0].a.img.src; //thumbnail image
+        entry["img"]=entry["thumb"].replace("thumbs/",""); //full size image  
+        entry["species"]=div.div[1].a.content; //scientific name
+        entry["name"]=div.div[2].p; //common name  
+        entry["commonNames"]=div.div[3].div.div[1].p.content; // all common names
+        entry["plantTypes"]=div.div[3].div.div[2].p; // plant types
+        entry["sunExposure"]=div.div[3].div.div[3].p; // sun exposure
+        entry["soilTexture"]=div.div[3].div.div[4].p; // soil texture
+        entry["soilMoisture"]=div.div[3].div.div[5].p; // soil moisture
+        entry["region"]=div.div[3].div.div[6].p; // region
+        
+        
+        // extract numeric plant id from url
+        plantid = entry["url"].split("/").pop(); 
+        
+        //remove leading punctuation
+        for (var x in entry) {
+          entry[x] = entry[x].replace(":  ","");
+          entry[x] = entry[x].replace(": ","");
+        }      
 
-            // save entry into mydata array
-            mydata[plantid]=entry;
-            
-            //create li
-            $listitem = $('<li>'); 
-            
-            //create link
-            $itemlink = $('<a>').attr({
-              "href":"#details-page",
-              "class":"search-result",
-              "id":plantid,
-              "data-transition":"slide"
-            }); 
-            
-            //create thumbnail image
-            $( "<img>").attr( "src", entry["thumb"] ).appendTo( $itemlink );
-            
-            // create html element with species name
-            $('<h3>', {text:entry["species"]}).appendTo( $itemlink ); 
+        // save entry into mydata array
+        mydata[plantid]=entry;
+        
+        //create li
+        $listitem = $('<li>'); 
+        
+        //create link
+        $itemlink = $('<a>').attr({
+          "href":"#details-page",
+          "class":"search-result",
+          "id":plantid,
+          "data-transition":"slide"
+        }); 
+        
+        //create thumbnail image
+        $( "<img>").attr( "src", entry["thumb"] ).appendTo( $itemlink );
+        
+        // create html element with species name
+        $('<h3>', {text:entry["species"]}).appendTo( $itemlink ); 
 
-            // create html element with name
-            $('<p>', {text:entry["name"]+entry["commonNames"]}).appendTo( $itemlink );
-            
-            // create html element plant type
-            $('<p>', {
-              text:"Plant Type: "+entry["plantTypes"],
-              style:"font-style:italic"
-            }).appendTo( $itemlink );
-
-              
-            // append link to list item
-            $itemlink.appendTo($listitem);   
-                      
-            // append row to main list
-            $('#mylist').append($listitem);
-            
-            // trigger jquery mobile with newly built list
-            $("#mylist").listview("refresh");
-            
-            // remove loading message
-            $.mobile.loading( 'hide');
-            
+        // create html element with name
+        $('<p>', {text:entry["name"]+entry["commonNames"]}).appendTo( $itemlink );
+ 
+        // create html element plant type
+        $('<p>', {
+          text:"Plant Type: "+entry["plantTypes"],
+          style:"font-style:italic"
+        }).appendTo( $itemlink );
+          
+        // append link to list item
+        $itemlink.appendTo($listitem);   
+                  
+        // append row to main list
+        $('#mylist').append($listitem);
+        
+        // trigger jquery mobile with newly built list
+        $("#mylist").listview("refresh");
+        
+        // remove loading message
+        $.mobile.loading( 'hide');
+        
       });
     });
   });
@@ -183,26 +180,16 @@ $(document).ready(function () {
       }
     });
     
-    
     // entry details plant info
     $('#entry-detail-b').append(
-      $('<div>').html('Common Names: <span style="font-weight:bold;">'+mydata[myplant].name+mydata[myplant].commonNames+"</div>"),
-      $('<div>').html('Plant Type: <span style="font-weight:bold;">'+mydata[myplant].plantTypes+"</div>"),
-      $('<div>').html('Sun Exposure: <span style="font-weight:bold;">'+mydata[myplant].sunExposure+"</div>"),
-      $('<div>').html('Soil Texture: <span style="font-weight:bold;">'+mydata[myplant].soilTexture+"</div>"),
-      $('<div>').html('Soil Moisture: <span style="font-weight:bold;">'+mydata[myplant].soilMoisture+"</div>"),
-      $('<div>').html('Region: <span style="font-weight:bold;">'+mydata[myplant].region+"</div>"),
+      $('<div>').html('Common Names: <span>'+mydata[myplant].name+mydata[myplant].commonNames+"</div>"),
+      $('<div>').html('Plant Type: <span>'+mydata[myplant].plantTypes+"</div>"),
+      $('<div>').html('Sun Exposure: <span>'+mydata[myplant].sunExposure+"</div>"),
+      $('<div>').html('Soil Texture: <span>'+mydata[myplant].soilTexture+"</div>"),
+      $('<div>').html('Soil Moisture: <span>'+mydata[myplant].soilMoisture+"</div>"),
+      $('<div>').html('Region: <span>'+mydata[myplant].region+"</div>"),
       $('<div>').html('<a href="'+mydata[myplant].url+'" class="ui-btn">Learn More</a>')
     );
-    
-    
-
   });
-  
 });
-
-
-
-
-
 
