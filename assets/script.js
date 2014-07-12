2cffbfa9f05d70432b879420b1edc74ab07e03a9
Mike Chelen <michael.chelen@gmail.com>
2014-07-12 19:16:48 -0400

    make opening screen seperate jqm page, closes #8 & #9

--- assets/script.js
+++ assets/script.js
@@ -4,29 +4,49 @@ $(document).ready(function () {
        
   // search form
   $("form").on("submit", function (e) {
-    
-    e.preventDefault(); // suppress form submit
-    
-    mydata = new Array(); // initialize with new array
-   
-    $('#mylist').empty(); // clear old results
-    
+
+//    var $form = $(this);
+//    var id = $form.attr('id');
+
+    console.log(this.id);
+
+    $.mobile.changePage('#search-results');
+
+    // jqm loading widget
     $.mobile.loading( 'show', {
       text: "Loading...",
       textVisible: true,
       theme: "b",
     });
 
-    var input = $('#query').val(); // get search form user input
-    var api = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.nativeplantcenter.net%2F%3Fq%3Ddatabase%26count%3D-1%26keyword%3D" + input + "%22%20and%20xpath%3D'%2F%2Fdiv%5Bcontains(%40class%2C%22database_entry%20matrix_entry%22)%5D'&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?"; // build yql & nativeplantcenter query
+    
+    e.preventDefault(); // suppress form submit
+    
+    mydata = new Array(); // initialize with new array
+   
+    $('#mylist').empty(); // clear old results from dom
 
+    // detect which search bar from either jqm page was used and sync input
+    if (this.id == "forma") {
+      var input = $('#querya').val(); // get search input from forma querya
+      $("#queryb").val(input); // sync querya to queryb
+    }
+    else {
+      var input = $('#queryb').val(); // get search input from formb queryb
+      $("#querya").val(input); // sync queryb to querya
+    }
+
+    
+    // build yql & nativeplantcenter query
+    var api = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fwww.nativeplantcenter.net%2F%3Fq%3Ddatabase%26count%3D-1%26keyword%3D" + input + "%22%20and%20xpath%3D'%2F%2Fdiv%5Bcontains(%40class%2C%22database_entry%20matrix_entry%22)%5D'&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=?";
+
+    // run jquery function
     $.getJSON(api, {
       format: "json"
-      // run jquery function
     })
     
-    .done(function (data) {
     // runs when $.getJSON() completes 
+    .done(function (data) {
   
       // handle case of 0 results found
       if (!data.query.results) {
@@ -118,6 +138,12 @@ $(document).ready(function () {
   // catch click on search results to show detailed page
   $(document).on('click', 'a.search-result', function() {
   
+//    console.log("foo");
+
+//    $.mobile.changePage('#details-page');
+
+//    console.log("bar");
+
     // clear old results
     $('#entry-detail-a').empty();
     $('#entry-detail-b').empty();
@@ -201,8 +227,8 @@ $(document).ready(function () {
     // get sample search from <a href="">
     var ssearch = $(this).text();
     console.log(ssearch);
-    $("#query").val(ssearch);
-    $("#query").submit();
+    $("#querya").val(ssearch);
+    $("#querya").submit();
   }
 );
 
